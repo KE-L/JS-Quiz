e2c8bc9fbeed3a6ade9db85eac2c1f1ee35b014c
kassy.essiet <jillanderson@air-21704.home>
2022-12-06 21:25:01 +0000

    added starter js code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,6 +3,7 @@ var startButton = document.getElementById('start');
 var submitButton = document.getElementById('submit');
 var initTracking = document.getElementById('initials');
 var feedbackTracking = document.getElementById('feedback');
+var questions = document.getElementById('questions');
 
 // Intro timer linked to start button
 var timerTracker = document.querySelector("time");
@@ -17,17 +18,38 @@ sendMessage();
 }, 
 1000);
 };
+// Intro faster increment if answer is incorrect
+
 // Init game & clear "start" screen 
 function initQuiz () {
-
 var startScreen = document.getElementById('start-screen');
 startScreen.classList.add("hide")
+questions.classList.remove("hide")
 }
 startButton.addEventListener('click', initQuiz) 
-// Intro faster increment if answer is incorrect
 
 
 // Intro questions 
+var questions;
+
+start_quiz() {
+  questions = 0;
+  show_questions 
+
+show_question() {
+  questionDisplay.textContent = questions[question].Question;
+  for (j = 0; j < 4; j++) {
+    var button = document.createElement('button')
+    button.innerText = questions[question].Answers[j]
+    answersDiv.appendChild(button);
+  }
+}
+
+// next_question() {
+//   question++;
+//   check if all questions have been asked then either show_question or end_quiz
+// }
+
 var questions = [
 {
 title: "Question 1",
@@ -60,15 +82,15 @@ answer: "",
 
 }, 
 
-// Intro methodology 
+// // Intro methodology 
 
 ];
 
-for (var i = 0; i < questions.length; i++){
-    console.log(questions[i])
-        for (var j = 0; j < questions[i].choices.length; j++)
-        {
-            console.log(questions[i].choices[j] + " " );
-        }
-};
+// for (var i = 0; i < questions.length; i++){
+//     console.log(questions[i])
+//         for (var j = 0; j < questions[i].choices.length; j++)
+//         {
+//             console.log(questions[i].choices[j] + " " );
+//         }
+// };
 
