7fe4fc9906ec8e5e0aea1ab01997c56114497deb
kassy.essiet <jillanderson@MacBook-Air-21704.local>
2022-12-06 14:15:01 +0000

    added tester Qs & calling variables

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,34 +1,49 @@
+var startButton = document.getElementById('start');
+var submitButton = document.getElementById('submit');
+var initTracking = document.getElementById('initials');
+var feedbackTracking = document.getElementById('feedback');
+
 var questions = [
 {
-title: "Question",
-choices: ["","","",""],
-answer: "",
+title: "Question 1",
+choices: ["A","b","c","d"],
+answer: "d",
 
 }
 , 
 {
-title: "Question",
-choices: ["","","",""],
-answer: "",
+title: "Question 2",
+choices: ["1","2","3","4"],
+answer: "4",
 },
 {
-title: "Question",
-choices: ["","","",""],
-answer: "",
+title: "Question 3",
+choices: ["data","set","match","flex"],
+answer: "flex",
 },
 {
-title: "Question",
-choices: ["","","",""],
-answer: "",
+title: "Question 4",
+choices: ["me","say","no","way"],
+answer: "say",
 
 }, 
 
 {
-title: "Question",
-choices: ["","","",""],
+title: "Question 5",
+choices: ["litty","titty","in","the","city"],
 answer: "",
 
 }, 
 
 
 ];
+
+for (var i = 0; i < questions.length; i++){
+    console.log(questions[i];
+        for (var j = 0; j < questions[i].choices.length; j++)
+        {
+            console.log(questions[i].choices[j] + " " );
+
+
+        }
+}
